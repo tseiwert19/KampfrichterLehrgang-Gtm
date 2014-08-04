363cb6f9dceed6ce3d39c54f59fb89ba9abeee5d
h41xy <urs.oberdorf@autistici.org>
2014-08-04 12:18:06 +0200

    InitialCommit fuer das Endergebniss auf Branch "development"

---
+++ KampfrichterLehrgang Gtm/src/src/main/Kampfrichterlehrgang.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Kampfrichterlehrgang {
+
+
+  public KampfrichterLehrgang () {
+
+  }
+
+  public static int main (String args[]) {
+
+  }
+}
