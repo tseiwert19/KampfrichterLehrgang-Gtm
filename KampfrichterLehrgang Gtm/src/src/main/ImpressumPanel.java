aeb3f63c9459aa5add778fc4428ac99c3ffb4e72
tseiwert19@googlemail.com <ts190390>
2014-08-15 17:58:18 +0200

    pdf öffnen in externer Änwendung

--- KampfrichterLehrgang Gtm/src/src/main/ImpressumPanel.java
+++ KampfrichterLehrgang Gtm/src/src/main/ImpressumPanel.java
@@ -2,15 +2,20 @@ package src.main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ImpressumPanel extends JPanel {
+public class ImpressumPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = -9115669597556271814L;
 
@@ -29,7 +34,7 @@ public class ImpressumPanel extends JPanel {
 		cdptext.setBackground(myRot);
 		cdptext.setBorderPainted(false);
 		cdptext.setOpaque(true);
-
+cdptext.addActionListener(this);
 		JPanel m = new JPanel();
 		m.setBackground(Color.WHITE);
 		JLabel l = new JLabel();
@@ -39,5 +44,15 @@ public class ImpressumPanel extends JPanel {
 		
 		
 		add(cdptext);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+try {
+	Desktop.getDesktop().open(new File("src/pdf/CDP_2013_GER_ENG_RUS_v2.pdf"));
+} catch (IOException e1) {
+	// TODO Auto-generated catch block
+	e1.printStackTrace();
+}		
 	}
 }
