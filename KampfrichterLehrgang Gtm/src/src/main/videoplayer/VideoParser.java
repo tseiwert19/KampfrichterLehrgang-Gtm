2357ba6610191e4d6bac369fa0b789b0fc4b7919
michael <michael@michael-System-Product-Name>
2014-08-23 20:06:29 +0200

    Refactoring + neue Methoden

--- KampfrichterLehrgang Gtm/src/src/main/videoplayer/VideoParser.java
+++ KampfrichterLehrgang Gtm/src/src/main/videoplayer/VideoParser.java
@@ -19,46 +19,56 @@ public class VideoParser {
 		}
 		
 		public ArrayList<Video> mappeVideosVonGeraet(String geraet){
-			ArrayList<Video> videos = new ArrayList<Video>();
 			ResultSet ergebnis = dbController.getAllByGeraet(geraet);
-		    int id;
-			String name;
-			String pfad;
-			String beschreibung;
-			String schwierigkeitsgrad;
-			String elementgruppe;
-			Video video;
-			
-			try {
-				while(ergebnis.next()){
-					id = ergebnis.getInt("id");
-					name = ergebnis.getString("name");
-					pfad = ergebnis.getString("pfad");
-					beschreibung = ergebnis.getString("beschreibung");
-					schwierigkeitsgrad = ergebnis.getString("schwierigkeitsgrad");
-					elementgruppe = ergebnis.getString("elementgruppe");
-					
-					video = new Video(id, name, pfad, geraet, beschreibung, schwierigkeitsgrad, elementgruppe);
-					videos.add(video);
-					
-				}
-			} catch (SQLException e) {
-				System.err.println("Fehler bei Datenbankabfrage!");
-				e.printStackTrace();
-			}
-			return videos;
+			return parseVideos(ergebnis);
 		}
 		
 		public ArrayList<Video> mappeVideosVonName(String name){
-		    ArrayList<Video> videos = new ArrayList<Video>();
             ResultSet ergebnis = dbController.getAllByName(name);
+            return parseVideos(ergebnis);
+		}
+		
+    public ArrayList<Video> mappeGefilterteVideos(String geraet, String schwierigkeitsgrad,
+            String elementgruppe)
+    {
+        ArrayList<Video> videos = null;
+        if (!schwierigkeitsgrad.equals("Alle Schwierigkeitsgrade anzeigen")
+                && !elementgruppe.equals("Alle Elementgruppen anzeigen"))
+        {
+            ResultSet ergebnis = dbController.getAllByGeraetSchwierigkeitsgradElementgruppe(geraet,
+                    schwierigkeitsgrad, elementgruppe);
+            videos = parseVideos(ergebnis);
+        }
+        else if (!schwierigkeitsgrad.equals("Alle Schwierigkeitsgrade anzeigen")
+                && elementgruppe.equals("Alle Elementgruppen anzeigen"))
+        {
+            ResultSet ergebnis = dbController.getAllByGeraetSchwierigkeitsgrad(geraet,
+                    schwierigkeitsgrad);
+            videos = parseVideos(ergebnis);
+        }
+        else if (schwierigkeitsgrad.equals("Alle Schwierigkeitsgrade anzeigen")
+                && !elementgruppe.equals("Alle Elementgruppen anzeigen"))
+        {
+            ResultSet ergebnis = dbController.getAllByGeraetElementgruppe(geraet, elementgruppe);
+            videos = parseVideos(ergebnis);
+        }else{
+            videos = mappeVideosVonGeraet(geraet);
+        }
+            
+
+        return videos;
+    }
+		
+		private ArrayList<Video> parseVideos(ResultSet ergebnis){
             int id;
-            String geraet;
+            String name;
             String pfad;
             String beschreibung;
+            String geraet;
             String schwierigkeitsgrad;
             String elementgruppe;
             Video video;
+            ArrayList<Video> videos = new ArrayList<Video>();
             
             try {
                 while(ergebnis.next()){
