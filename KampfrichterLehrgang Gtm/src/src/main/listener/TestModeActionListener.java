c7d8c6fd51e2cd7d9557df4848554c5cdba7e53e
Michael <Michael@Michael-PC>
2014-09-10 15:55:02 +0200

    umbenannt

---
+++ KampfrichterLehrgang Gtm/src/src/main/listener/TestModeActionListener.java
@@ -0,0 +1,41 @@
+package src.main.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import src.main.Controller;
+import src.main.components.KariButton;
+import src.main.panel.TestModePanel;
+
+
+public class TestModeActionListener implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        String command = e.getActionCommand();
+        KariButton button = (KariButton) e.getSource();
+        String name = button.getText();
+        TestModePanel testPanel = Controller.getTestPanel();
+        
+        switch (name) {
+            case    "Start":
+                    testPanel.versteckeModusPanel();
+                    testPanel.starteTest();
+                break;
+            case    "Nächste Aufgabe":
+                    testPanel.naechstesVideo();
+                break;
+            default:
+                if(command.equals(name)){
+                    testPanel.zeigeRichtigesErgebnis(command);
+                }else
+                    testPanel.zeigeFalschesErgebnis(command);
+                break;
+        
+        }
+        
+    }
+
+}
