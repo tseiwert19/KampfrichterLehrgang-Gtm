4358413e24b4673e241f1cb15891cdb967950cca
Michael <Michael@Michael-PC>
2014-09-11 11:51:22 +0200

    Klammer bei Else-Zweig hinzugefügt

--- KampfrichterLehrgang Gtm/src/src/main/listener/TestModeActionListener.java
+++ KampfrichterLehrgang Gtm/src/src/main/listener/TestModeActionListener.java
@@ -7,39 +7,36 @@ import src.main.Controller;
 import src.main.components.KariButton;
 import src.main.panel.TestModePanel;
 
+public class TestModeActionListener implements ActionListener {
 
-public class TestModeActionListener implements ActionListener
-{
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		KariButton button = (KariButton) e.getSource();
+		String name = button.getText();
+		TestModePanel testPanel = Controller.getTestModePanel();
 
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        String command = e.getActionCommand();
-        KariButton button = (KariButton) e.getSource();
-        String name = button.getText();
-        TestModePanel testPanel = Controller.getTestModePanel();
-        
-        switch (command) {
-            case    "start":
-                    testPanel.versteckeModusPanel();
-                    testPanel.starteTest();
-                break;
-            case    "next":
-                    testPanel.naechstesVideo();
-                break;
-            case	"new":
-            		System.out.println("NEUER TEST");
-            		testPanel.neuerTestVorbereiten();
-            	break;
-            default:
-                if(command.equals(name)){
-                    testPanel.zeigeRichtigesErgebnis(command);
-                }else
-                    testPanel.zeigeFalschesErgebnis(command);
-                break;
-        
-        }
-        
-    }
+		switch (command) {
+		case "start":
+			testPanel.versteckeModusPanel();
+			testPanel.starteTest();
+			break;
+		case "next":
+			testPanel.naechstesVideo();
+			break;
+		case "new":
+			System.out.println("NEUER TEST");
+			testPanel.neuerTestVorbereiten();
+			break;
+		default:
+			if (command.equals(name)) {
+				testPanel.zeigeRichtigesErgebnis(command);
+				testPanel.addTabelleZeile(name, command, true);
+			} else{
+				testPanel.zeigeFalschesErgebnis(command);
+				testPanel.addTabelleZeile(name, command, false);
+			}
+		}
+	}
 
 }
