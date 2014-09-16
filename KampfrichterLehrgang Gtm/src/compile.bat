cccfb9f2d797ef94084c2a558660d67cdd488fa4
Christopher Amador <trachused@safe-mail.net>
2014-09-16 17:04:28 +0200

    Anpassungen für das Ausführen mit JDK unter Windows-64.

--- KampfrichterLehrgang Gtm/src/compile.bat
+++ KampfrichterLehrgang Gtm/src/compile.bat
@@ -2,7 +2,8 @@
 
 del /F /S /Q *.class
 
-for /R %ProgramFiles% %%I in (javac.exe*) do set JAVA_HOME=%%~dpI
+REM http://techdem.centerkey.com/2009/05/javahome-command-script.html
+for /R "%ProgramFiles%" %%I in (javac.exe*) do set JAVA_HOME=%%~dpI
 set PATH=%PATH%;%JAVA_HOME%
 javac -Djava.ext.dirs=src/main/libs src/main/Kampfrichterlehrgang.java
 
