cc5cd7ec851cb89ff8c1551564d0861888d14f58
Christopher Amador <trachused@safe-mail.net>
2014-09-19 14:09:32 +0200

    Kleinigkeiten.

--- KampfrichterLehrgang Gtm/src/compile.bat
+++ KampfrichterLehrgang Gtm/src/compile.bat
@@ -7,4 +7,8 @@ for /R "%ProgramFiles%" %%I in (javac.exe*) do set JAVA_HOME=%%~dpI
 set PATH=%PATH%;%JAVA_HOME%
 javac -encoding UTF-8 -Djava.ext.dirs=src/main/libs src/main/Kampfrichterlehrgang.java
 
-7z a -tzip ../../"KampfrichterLehrgang Gtm.zip" ../../"KampfrichterLehrgang Gtm"
+
+del ..\..\"KampfrichterLehrgang Gtm.zip"
+for /R "%ProgramFiles%" %%I in (7z.exe*) do set SEVENZIP_HOME=%%~dpI
+set PATH=%PATH%;%SEVENZIP_HOME%
+7z a -tzip ..\..\"KampfrichterLehrgang Gtm.zip" ..\..\"KampfrichterLehrgang Gtm"
